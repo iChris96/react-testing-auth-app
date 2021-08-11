051a2525a23c3437ef2280ceaccbba6042fae935
christopher <christopher_x10x@hotmail.com>
2021-08-11 15:21:31 -0500

    testing requiered email and password messages for empty form values

--- src/auth/components/login-page/login-page.js
+++ src/auth/components/login-page/login-page.js
@@ -1,13 +1,30 @@
 import {Button, TextField} from '@material-ui/core'
-import React from 'react'
+import React, {useState} from 'react'
 
-const LoginPage = () => (
-  <>
-    <h1>Login Page</h1>
-    <TextField label="email" id="email" />
-    <TextField label="password" id="password" />
-    <Button>Send</Button>
-  </>
-)
+const LoginPage = () => {
+  const [emailTextValitaion, setEmailTextValitaion] = useState('')
+  const [passwordTextValitaion, setPasswordTextValitaion] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setEmailTextValitaion('the email is required')
+    setPasswordTextValitaion('the password is required')
+  }
+
+  return (
+    <>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField label="email" id="email" helperText={emailTextValitaion} />
+        <TextField
+          label="password"
+          id="password"
+          helperText={passwordTextValitaion}
+        />
+        <Button type="submit">Send</Button>
+      </form>
+    </>
+  )
+}
 
 export default LoginPage
--- src/auth/components/login-page/login-page.test.js
+++ src/auth/components/login-page/login-page.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {screen, render} from '@testing-library/react'
+import {screen, render, fireEvent} from '@testing-library/react'
 
 import LoginPage from './login-page'
 
-describe('when login page is mounted', () => {
-  beforeEach(() => render(<LoginPage />))
+beforeEach(() => render(<LoginPage />))
 
+describe('when login page is mounted', () => {
   it('must display the login title', () => {
     const text = screen.getByText(/login page/i)
     expect(text).toBeInTheDocument()
@@ -15,5 +15,23 @@ describe('when login page is mounted', () => {
     expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
     expect(screen.getByRole('button', {name: /send/i})).toBeInTheDocument()
+  })
+})
+
+describe('when the user leaves empty fields and clicks the submit button', () => {
+  it('display required messages as the format: "The [field name] is required"', () => {
+    expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument() // querybytext doesn't brake the text if the element is not founded
+    expect(
+      screen.queryByText(/the password is required/i),
+    ).not.toBeInTheDocument()
+
+    // screen.debug()
+
+    const sendBtn = screen.getByRole('button', {name: /send/i})
+
+    fireEvent.click(sendBtn)
+
+    expect(screen.getByText(/the email is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/the password is required/i)).toBeInTheDocument()
   })
 })
