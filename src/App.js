53a29d189807b899d89f116198d800e7fa241101
christopher <christopher_x10x@hotmail.com>
2021-08-24 15:59:12 -0500

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import LoginPage from './auth/components/login-page/login-page'
 
+const Home = () => <h1>Home Route</h1>
+
 function App() {
-  return <LoginPage />
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
