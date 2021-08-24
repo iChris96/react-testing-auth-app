cfd17f0708b1e7d4c5ad36e622b5aed0c17ed243
christopher <christopher_x10x@hotmail.com>
2021-08-23 21:15:20 -0500

    handling UI

--- src/auth/components/login-page/login-page.js
+++ src/auth/components/login-page/login-page.js
@@ -1,5 +1,36 @@
-import {Button, CircularProgress, Snackbar, TextField} from '@material-ui/core'
+import {
+  Button,
+  CircularProgress,
+  Container,
+  Snackbar,
+  TextField,
+  Typography,
+  CssBaseline,
+  Avatar,
+} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import React, {useState} from 'react'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
 
 const validateEmail = email => {
   const regex = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/
@@ -24,6 +55,7 @@ const login = ({email, password}) =>
   })
 
 const LoginPage = () => {
+  const classes = useStyles()
   const [emailTextValitaion, setEmailTextValitaion] = useState('')
   const [passwordTextValitaion, setPasswordTextValitaion] = useState('')
   const [formValues, setFormValues] = useState({email: '', password: ''})
@@ -93,43 +125,65 @@ const LoginPage = () => {
   }
 
   return (
-    <>
-      <h1>Login Page</h1>
-      {isFetching && <CircularProgress data-testid="loading-indicator" />}
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="email"
-          id="email"
-          name="email"
-          helperText={emailTextValitaion}
-          onChange={handleChange}
-          onBlur={handleBlurEmail}
-          value={formValues.email}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login Page
+        </Typography>
+        {isFetching && <CircularProgress data-testid="loading-indicator" />}
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="email"
+            id="email"
+            name="email"
+            helperText={emailTextValitaion}
+            onChange={handleChange}
+            onBlur={handleBlurEmail}
+            value={formValues.email}
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <TextField
+            label="password"
+            id="password"
+            name="password"
+            helperText={passwordTextValitaion}
+            onChange={handleChange}
+            onBlur={handleBlurPassword}
+            value={formValues.password}
+            type="password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+          />
+          <Button
+            type="submit"
+            disabled={isFetching}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Send
+          </Button>
+        </form>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={isSnackOpen}
+          autoHideDuration={6000}
+          onClose={() => setIsSnackOpen(false)}
+          message={snackMessage}
         />
-        <TextField
-          label="password"
-          id="password"
-          name="password"
-          helperText={passwordTextValitaion}
-          onChange={handleChange}
-          onBlur={handleBlurPassword}
-          value={formValues.password}
-        />
-        <Button type="submit" disabled={isFetching}>
-          Send
-        </Button>
-      </form>
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        open={isSnackOpen}
-        autoHideDuration={6000}
-        onClose={() => setIsSnackOpen(false)}
-        message={snackMessage}
-      />
-    </>
+      </div>
+    </Container>
   )
 }
 
