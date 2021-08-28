45bfa30e3c4eeebb9052bb789544945bfb7c0095
christopher <christopher_x10x@hotmail.com>
2021-08-28 16:08:33 -0500

    adding button to go employeee page from admin page |  handling employee page ui

--- src/auth/components/employees-page/employeees-page.js
+++ src/auth/components/employees-page/employeees-page.js
@@ -1,26 +1,22 @@
-import {AppBar, Button, Toolbar, Typography} from '@material-ui/core'
+import {AppBar, Toolbar, Typography} from '@material-ui/core'
 import React, {useContext} from 'react'
-import {Link} from 'react-router-dom'
 import AuthContext from '../../../ultils/contexts/auth-context'
 
-const AdminPage = () => {
+const EmployeePage = () => {
   const {user} = useContext(AuthContext)
   return (
     <>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">{user.name}</Typography>
-          <Button color="inherit" component={Link} to="/employee">
-            Employees
-          </Button>
         </Toolbar>
       </AppBar>
 
       <Typography component="h1" variant="h5">
-        Admin page
+        Employees page
       </Typography>
     </>
   )
 }
 
-export default AdminPage
+export default EmployeePage
